aa0cb4f8b985a3fa63b52b075ab215d207995261
Gilbmet <gilbmet254@gmail.com>
2022-10-24 01:24:20 +0300

    Update handle_print.c
    updated

--- handle_print.c
+++ handle_print.c
@@ -1,15 +1,14 @@
 #include "main.h"
-
 /**
- * handle_print - prints an argument depending on its type
- * @fmt: formatted string in which to print the arguments
- * @list: list of arguments to be printed
- * @ind: ind
- * @buffer: buffer array
- * @flags: calculates active flags
- * @width: get width
- * @precision: precision specification
- * @size: size specifier
+ * handle_print - Prints an argument based on its type
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
+ * @ind: ind.
+ * @buffer: Buffer array to handle print.
+ * @flags: Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
  *
  * Return: 1 or 2;
  */
@@ -19,14 +18,14 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
-		{'i', print_unsigned}, {'d', print_int}, {'b', print_binary},
+		{'i', print_int}, {'d', print_int}, {'b', print_binary},
 		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
-			return (fmt_types[i].fn(list, buffer, flags, width, precision, size)):
+			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 
 	if (fmt_types[i].fmt == '\0')
 	{
